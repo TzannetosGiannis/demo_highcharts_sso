ef5657589ea2fa9e63e161062558394e32be7545
Ioannis Tzannetos <tzannetosg@gmail.com>
2023-05-04 19:28:24 +0300

    demo with highcharts and sso

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import { useSession, signIn, signOut } from 'next-auth/react';
+import { useRouter } from 'next/router';
+export default function IndexPage() {
+const router = useRouter();
+const { data, status } = useSession();
+if (status === 'loading') return <h1> loading... please wait</h1>;
+if (status === 'authenticated') {
+return (
+<div>
+<h1> hi {data.user.name}</h1>
+<img src={data.user.image} alt={data.user.name + ' photo'} />
+<br></br>
+<button onClick={() => { router.push('/charts'); }}>Charts</button>
+<button onClick={signOut}>sign out</button>
+</div>
+);
+}
+return (
+<div>
+<button onClick={() => signIn('google')}>sign in with gooogle</button>
+<button onClick={() => { router.push('/charts'); }}>Charts</button>
+</div>
+);
+}
+
